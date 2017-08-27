1cab5a43c297c224181c79a20c6afc0cd1c7f53d
Hamster Tian <haotia@gmail.com>
2017-08-27 15:51:54 +0000

    sagit: Squashed updates for media configs
    commit 1fe9fc66efa4e332bcc0189c8afdc4260153970b
    Author: Hamster Tian <haotia@gmail.com>
    Date:   Sun Aug 27 15:51:54 2017 +0000
    
        op5: use media_codec and _profile from marlin to get working media
    
        * Wrong media_profiles also causes bootloop.
    
    commit f96a008478759da494f242a44c0118cb3ba75f2d
    Author: Simon Shields <simon@lineageos.org>
    Date:   Sun Aug 27 14:06:53 2017 +0200
    
        op5: media: remove unsupported VGA and 4K DCI encoder profiles
    
    commit 8d4ad06a6c39cd6a2d357a2f39065519afe19bc4
    Author: Hamster Tian <haotia@gmail.com>
    Date:   Sun Sep 3 19:01:53 2017 +0800
    
        op5: remove more unsupported codecs / profiles
    
        * 2k, qhd, lpcm
    
    commit d02591964997aef37d1df0ff8ed7c4b003da2a90
    Author: Hamster Tian <haotia@gmail.com>
    Date:   Sun Sep 3 19:23:12 2017 +0800
    
        op5: restore media_codecs and remove incompatible limits
    
        * vt-* are unsupported, cause the whole file fail to load.
        * Remove ffmpeg as well
        * This does speed things up a bit...
    
    Change-Id: I28497aae9923b6449e18983c677b707bfe64bb15

--- device.mk
+++ device.mk
@@ -228,14 +228,14 @@ PRODUCT_PACKAGES += \
 
 # Media
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/media_codecs.xml:system/etc/media_codecs.xml \
-    $(LOCAL_PATH)/configs/media_codecs_performance.xml:system/etc/media_codecs_performance.xml \
-    $(LOCAL_PATH)/configs/media_profiles.xml:system/etc/media_profiles.xml
+    $(LOCAL_PATH)/configs/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
+    $(LOCAL_PATH)/configs/media_codecs_performance.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_performance.xml \
+    $(LOCAL_PATH)/configs/media_profiles_V1_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_V1_0.xml
 
 PRODUCT_COPY_FILES += \
-    frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
-    frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
-    frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml
+    frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_audio.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_telephony.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video.xml
 
 # Media Extensions
 PRODUCT_PACKAGES += \
