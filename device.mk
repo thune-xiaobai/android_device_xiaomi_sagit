79327891bc4db331ca5efd7962a39f9e4399902c
Alexander Martinz <alex@amartinz.at>
2017-09-25 10:57:01 +0200

    sagit: media: adjust configurations and flags
    * do not build legacy hal
    * enable treble omx
    * move media codecs to vendor
    * set media audio permissions for /sys/kernel/wcd_cpe0/fw_name
    * build media extensions
    
    Change-Id: I6e03d63eef1eb5d7d8efbcda8a7c81ebe5fb547b
    Signed-off-by: Alexander Martinz <alex@amartinz.at>

--- device.mk
+++ device.mk
@@ -237,6 +237,13 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml
 
+# Media Extensions
+PRODUCT_PACKAGES += \
+    libavmediaserviceextensions \
+    libmediametrics \
+    libregistermsext \
+    mediametrics \
+
 # NFC
 PRODUCT_PACKAGES += \
     android.hardware.nfc@1.0-impl \
