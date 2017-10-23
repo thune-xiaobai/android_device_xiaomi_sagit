f1c82a16eeb9436ff57befdbaf767fe46e21a65a
xiaobai <fengz181x@126.com>
2017-10-23 14:58:58 +0800

    sagit: add HIDL manifests

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -184,6 +184,10 @@ TARGET_FS_CONFIG_GEN := $(DEVICE_PATH)/config.fs
 TARGET_NO_RPC := true
 USE_DEVICE_SPECIFIC_GPS := true
 
+# HIDL
+DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
+DEVICE_MATRIX_FILE := $(DEVICE_PATH)/compatibility_matrix.xml
+
 # Init
 TARGET_PLATFORM_DEVICE_BASE := /devices/soc/
 
