a26da3812c287da7921a02d73b0a29e99a0c0f1a
Hamster Tian <haotia@gmail.com>
2017-12-25 10:20:51 +0000

    sagit: update graphics configuration to match Oreo blobs
    * libdisplayconfig is in display HAL (LA.UM.6.4)
    
    Change-Id: Iaa152431ed751dc20ffa4dd70566c6a362f14018

--- device.mk
+++ device.mk
@@ -148,6 +148,9 @@ PRODUCT_PACKAGES += \
     gralloc.msm8998 \
     hwcomposer.msm8998 \
     memtrack.msm8998 \
+    libdisplayconfig \
+    libhwc2on1adapter \
+    libgenlock \
     liboverlay \
     libtinyxml \
     android.hardware.graphics.allocator@2.0-impl \
