795898ba09878b7b332b6bf53d1f309b077a58c0
dianlujitao <dianlujitao@lineageos.org>
2018-01-20 18:40:23 +0800

    sagit: Add powerhint.xml
     * From MIUI dev 8.2.1
     * Required by perf_hint in libqti-perfd-client.so.
    
    Change-Id: Ibcd7eb642622d91a5634b6abd3bae3a386c96bc6

--- device.mk
+++ device.mk
@@ -306,6 +306,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.power@1.1-service-qti
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
+
 # QMI
 PRODUCT_PACKAGES += \
     libjson
