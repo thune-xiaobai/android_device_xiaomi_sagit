6e83677ac40ecfb0cfd2eede898ed1e4a77fb1ed
Bruno Martins <bgcngm@gmail.com>
2018-01-29 21:07:02 +0000

    sagit: Ship additional textclassifier's smart selection models
    Change-Id: I2a89dd8fe9918d00e02e06313b6e9087d9e5b07a

--- device.mk
+++ device.mk
@@ -368,6 +368,10 @@ PRODUCT_PACKAGES += \
     android.hardware.thermal@1.0-impl \
     android.hardware.thermal@1.0-service
 
+# TextClassifier
+PRODUCT_PACKAGES += \
+    textclassifier.smartselection.bundle1
+
 #USB HAL
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service
