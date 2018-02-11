4a18b47c9abf3c6743dc255d51fb9565edb37ee1
codeworkx <daniel.hillenbrand@codeworkx.de>
2018-02-11 20:24:24 +0100

    sagit: build libqdMetaData.system for ims
    Change-Id: Ia4fc32846600eafc2dedc6ae6a2d829c1f073163

--- device.mk
+++ device.mk
@@ -161,6 +161,8 @@ PRODUCT_PACKAGES += \
     libgenlock \
     liboverlay \
     libtinyxml \
+    libqdMetaData \
+    libqdMetaData.system \
     android.hardware.graphics.allocator@2.0-impl \
     android.hardware.graphics.allocator@2.0-service \
     android.hardware.graphics.mapper@2.0-impl \
