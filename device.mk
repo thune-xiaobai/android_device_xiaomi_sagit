0fff53c66b9de41a3b34244b74922ebd03fc941a
xiaobai <fengz181x@126.com>
2018-02-26 15:53:09 +0800

    sagit: move sec_config to vendor

--- device.mk
+++ device.mk
@@ -237,7 +237,7 @@ PRODUCT_COPY_FILES += \
 
 # IRSC
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/sec_config:system/etc/sec_config
+    $(LOCAL_PATH)/configs/sec_config:$(TARGET_COPY_OUT_VENDOR)/etc/sec_config
 
 # Keymaster HAL
 PRODUCT_PACKAGES += \
