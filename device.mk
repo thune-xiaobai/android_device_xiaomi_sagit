81bf300776fe7853dc3e15848c68778aa07d5275
xiaobai <fengz181x@126.com>
2018-02-26 16:10:59 +0800

    sagit: build libshim_MiCamera

--- device.mk
+++ device.mk
@@ -129,6 +129,7 @@ PRODUCT_PACKAGES += \
 # Camera
 PRODUCT_PACKAGES += \
     Snap \
+    libshim_MiCamera \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
     camera.device@3.2-impl \
