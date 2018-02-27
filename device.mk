8b11a91b12651661601a07d2ec657cdc2fe95882
xiaobai <fengz181x@126.com>
2018-02-27 17:42:36 +0800

    sagit: enable binderized display.qservice

--- device.mk
+++ device.mk
@@ -164,7 +164,8 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-service \
     android.hardware.graphics.mapper@2.0-impl \
     android.hardware.memtrack@1.0-impl \
-    android.hardware.graphics.composer@2.1-impl
+    android.hardware.graphics.composer@2.1-impl \
+    android.hardware.graphics.composer@2.1-service
 
 # Doze
 PRODUCT_PACKAGES += \
