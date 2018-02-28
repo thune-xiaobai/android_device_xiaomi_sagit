3cd9d3b63e5dd12ef664c77743f7ebfe96306ab3
xiaobai <fengz181x@126.com>
2018-02-28 15:23:11 +0800

    sagit: move camera shim to BoardConfig
    Change-Id: Idec1b9a60c9eafeeb2206af077610e38df229846

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -248,6 +248,9 @@ include device/qcom/sepolicy/sepolicy.mk
 # Sensors
 USE_SENSOR_MULTI_HAL := true
 
+# SHIMS
+TARGET_LD_SHIM_LIBS := /system/lib/libMiCameraHal.so|libshim_MiCamera.so
+
 # Vendor init
 TARGET_INIT_VENDOR_LIB := libinit_sagit
 TARGET_RECOVERY_DEVICE_MODULES := libinit_sagit
