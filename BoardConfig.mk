28b3fb10823fad4345b95e49cbe065fb18e8c615
xiaobai <fengz181x@126.com>
2018-03-02 19:35:34 +0800

    sagit: use Snapdragon LLVM, if available

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -19,6 +19,9 @@ DEVICE_PATH := device/xiaomi/sagit
 
 TARGET_SPECIFIC_HEADER_PATH := $(DEVICE_PATH)/include
 
+# Use Snapdragon LLVM, if available
+TARGET_USE_SDCLANG := true
+
 # Architecture
 TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-a
