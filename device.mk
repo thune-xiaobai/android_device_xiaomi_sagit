031483c2613fc7b5bbfac89bef8ed6d7fa93131c
Demon Singur <demonsingur@gmail.com>
2018-03-04 17:25:12 +0000

    sagit: turn fingerprint wrapper intro a hidl service
    Change-Id: Ib72892417d401947aeb3935ef0f018c3b5daf94b

--- device.mk
+++ device.mk
@@ -184,7 +184,7 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint sensor
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.1-service
+    android.hardware.biometrics.fingerprint@2.1-service.xiaomi_msm8998
 
 # For config.fs
 PRODUCT_PACKAGES += \
