4a9bed2b20f72e4a0c33f5f77e1382eae0aeef43
Bruno Martins <bgcngm@gmail.com>
2018-04-09 23:38:58 +0100

    sagit: Drop useless CNE related flag
     * Nothing in the OSS bits takes it into account.
    
    Change-Id: I6f949a12c643810cb1fba764bf13abb30e5dd88c

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -148,9 +148,6 @@ USE_DEVICE_SPECIFIC_CAMERA := true
 # Charger
 BOARD_CHARGER_ENABLE_SUSPEND := true
 
-# CNE and DPM
-BOARD_USES_QCNE := true
-
 # Dex
 ifeq ($(HOST_OS),linux)
   ifneq ($(TARGET_BUILD_VARIANT),eng)
